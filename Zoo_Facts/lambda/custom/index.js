75014fd81c9d38f3caa63056b8206188e6450e36
Dan Thompson <thompson.daniel@gmail.com>
2017-11-12 00:18:28 -0600

    speech POC in place.

--- Zoo_Facts/lambda/custom/index.js
+++ Zoo_Facts/lambda/custom/index.js
@@ -1,70 +1,57 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
+
+const DEV = process.env.NODE_ENV !== 'production';
+
+const contentful = require('contentful')
+const client = contentful.createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.CDA_TOKEN
+});
+
+var languageStrings="";
+
+if (DEV) {
+  client.getEntries({'sys.id': process.env.ROOT_ENTRY, 'include': 2}).then((response) => {
+    languageStrings = response.items[0].fields;
+    console.log(JSON.stringify(languageStrings, null, 4));
+  })
+}
 
 const Alexa = require('alexa-sdk');
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
-
-const languageStrings = {
-    'en': {
-        translation: {
-            FACTS: [
-                'Here is a test fact',
-            ],
-            SKILL_NAME: 'Zoo Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
-            HELP_MESSAGE: 'You can say tell me a zoo fact, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    }
-};
-
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random zoo fact from the zoo facts list
-        // Use this.t() to get corresponding language data
-        const factArr = this.t('FACTS');
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
+  'LaunchRequest': function() {
+    this.emit('GetFact');
+  },
+  'GetNewFactIntent': function() {
+    this.emit('GetFact');
+  },
+  'GetFact': function() {
+    const factIndex = Math.floor(Math.random() * languageStrings.facts.length);
+    const randomFact = languageStrings.facts[factIndex].fields.textContent;
 
-        // Create speech output
-        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = this.t('HELP_MESSAGE');
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
+    // Create speech output
+    const speechOutput = languageStrings.ctaText + " " + randomFact;
+    this.emit(':tellWithCard', speechOutput, languageStrings.headline, randomFact);
+  },
+  'AMAZON.HelpIntent': function() {
+    const speechOutput = languageStrings.helpMessage;
+    const reprompt = languageStrings.helpMessage;
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function() {
+    this.emit(':tell', languageStrings.stopMessage);
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit(':tell', languageStrings.stopMessage);
+  }
 };
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
+exports.handler = function(event, context) {
+  const alexa = Alexa.handler(event, context);
+  client.getEntries({'sys.id': process.env.ROOT_ENTRY, 'include': 2}).then((response) => {
+    languageStrings = response.items[0].fields;
     alexa.registerHandlers(handlers);
     alexa.execute();
+  })
 };
